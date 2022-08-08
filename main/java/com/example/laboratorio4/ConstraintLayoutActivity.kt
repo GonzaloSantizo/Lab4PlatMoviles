9e173f5f7c8a9c7cea808ea3f10323796ea276ab
GonzaloSantizo <84192252+GonzaloSantizo@users.noreply.github.com>
2022-08-08 17:03:11 -0600

    Entrega lab 4
    Added files for turn in

---
+++ main/java/com/example/laboratorio4/ConstraintLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.laboratorio4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstraintLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.constraint_layout)
+    }
+}
